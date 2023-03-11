0ffed9ca633046d086d24760aeaa2fb719b31f84
Daniil Osokin <daniil.osockin@gmail.com>
2023-03-11 23:39:28 +0400

    Renamed waveform.py to sample_waveform.py

--- src/sample_waveform.py
+++ src/sample_waveform.py
@@ -3,16 +3,20 @@ import librosa.display
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from numpy import ndarray
 
 
 class SampleWaveform(tk.Frame):
-    def __init__(self, master: tk.Misc, sound, sample_rate):
+    _SCALE_X = 20
+    _SCALE_Y = 2
+
+    def __init__(self, master: tk.Misc, duration: int):
         super().__init__(master)
 
         self.canvas_frame = tk.Frame(self.master)
-        self.canvas_frame.pack(fill="both")
+        self.canvas_frame.pack(fill="both", expand=True)
 
-        self.fig = Figure(figsize=(len(sound) / sample_rate / 20, 2), dpi=100)
+        self.fig = Figure(figsize=(duration / 1000 / self._SCALE_X, self._SCALE_Y), dpi=100)
         self.fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
         self.ax = self.fig.add_subplot()
 
@@ -22,7 +26,10 @@ class SampleWaveform(tk.Frame):
         self.canvas = FigureCanvasTkAgg(self.fig, self.canvas_frame)
         self.canvas.get_tk_widget().grid(column=0, row=0, sticky="nsew")
 
-        self.adaptor = librosa.display.waveshow(sound, sr=sample_rate, ax=self.ax, axis="s", lw=0.5, zorder=1)
+    def update_waveform(self, sound: ndarray, sample_rate: int):
+        self.ax.clear()
+
+        librosa.display.waveshow(sound, sr=sample_rate, ax=self.ax, axis="s", lw=0.5, zorder=1)
 
         self.ax.set_xlim(0, len(sound) / sample_rate)
         self.canvas.draw()
